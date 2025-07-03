4e0d28a9c2fa034e41ae0b3f86075266454b12a2
Gouravbirwaz <gouravbirwaz@gmail.com>
2025-07-03 22:50:50 +0530

    new chages

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,14 @@ import { getStorage } from 'firebase/storage'; // Ensure you import getStorage f
 
 // Your Firebase configuration
 const firebaseConfig = {
-  //firebase configuration;
+  apiKey: "AIzaSyBFHxt83jUiWNzhUSX63qcq6xDVCsWUBlw",
+  authDomain: "gemhack-a9246.firebaseapp.com",
+  databaseURL: "https://gemhack-a9246-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "gemhack-a9246",
+  storageBucket: "gemhack-a9246.firebasestorage.app",
+  messagingSenderId: "118483115946",
+  appId: "1:118483115946:web:65c9b5e60c7a123c86d5d3",
+  measurementId: "G-Y075DR7QNE"
 };
 
 // Initialize Firebase app
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client'; // ✅ Use 'react-dom/client' for React 18+
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
 import App from './App';
 
-ReactDOM.render(
-  <I18nextProvider i18n={i18n}>
-    <App />
-  </I18nextProvider>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
 );
